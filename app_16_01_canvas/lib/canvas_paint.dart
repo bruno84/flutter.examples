32d6620c830a3ed47b1cd89670ce0e479da2bd78
Bruno <bruno84@gmail.com>
2023-10-04 12:32:19 -0300

    poucas alterações

--- app_16_01_canvas/lib/canvas_paint.dart
+++ app_16_01_canvas/lib/canvas_paint.dart
@@ -6,33 +6,52 @@ class CanvasPaint extends CustomPainter
   @override
   void paint(Canvas canvas, Size size)
   {
-    // Paint
-    final paint = Paint();
-    paint.style = PaintingStyle.stroke;
-    paint.strokeWidth = 4.0;
-    paint.color = Colors.red;
+    //--------------------------------------------------------
+    // Paint1
+    //--------------------------------------------------------
+    final paint1 = Paint();
+    paint1.style = PaintingStyle.stroke;
+    paint1.strokeWidth = 4.0;
+    paint1.color = Colors.red;
 
     // RRect
     canvas.drawRRect(
       RRect.fromRectAndRadius(Rect.fromLTWH(0, 20, 100, 150), Radius.circular(20)),
-      paint,
+      paint1,
     );
 
     // Oval
     canvas.drawOval(
       Rect.fromLTWH(size.width - 150, 0, 100, 120),
-      paint,
+      paint1,
     );
 
-    paint.color = Colors.yellow;
+    // A alteração refletirá apenas nas novas primitivas
+    paint1.color = Colors.yellow;
 
     // Path
     final path = Path();
     path.moveTo(size.width * 0.5, size.height * 0.5);
     path.lineTo(size.width, size.height-20);
     path.lineTo(0, size.height-20);
-    canvas.drawPath(path, paint);
+    canvas.drawPath(path, paint1);
+
+    //--------------------------------------------------------
+    // Paint2
+    //--------------------------------------------------------
+    final paint2 = Paint();
+    paint2.color = Colors.orange;
+    paint1.style = PaintingStyle.fill;
+
+    // Oval
+    canvas.drawOval(
+      Rect.fromLTWH(0, 200, 50, 50),
+      paint2,
+    );
+
   }
+
+
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
